65742448f51ea3e1138e1c7a4632256ea9d2b53d
Loganathan M <Loganathan_M3@Dellteam.com>
2021-05-17 23:25:21 +0530

    Producer-Consumer App using RabbitMQ

---
+++ Producer/Sender.cs
@@ -0,0 +1,29 @@
+﻿using RabbitMQ.Client;
+using System;
+using System.Text;
+
+namespace Producer
+{
+    class Sender
+    {
+        static void Main(string[] args)
+        {
+            var factory = new ConnectionFactory() { HostName = "localhost" };
+
+            using var connection = factory.CreateConnection();
+            using var channel = connection.CreateModel();
+            {
+                channel.QueueDeclare("WWII", false, false, false, null);
+
+                string message = "Hitler's is about to send troops to Stanlingrad";
+                var body = Encoding.UTF8.GetBytes(message);
+
+                channel.BasicPublish("", "WWII", null, body);
+                Console.WriteLine($"Sent message - {message} ");
+            }
+
+            Console.WriteLine("Press [enter] to exit the sender app...");
+            Console.ReadLine();
+        }
+    }
+}
